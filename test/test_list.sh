91a8d2ec411fb4e752cd010c555e01aa101a7ca5
iamhsa <hugosalesses@gmail.com>
2017-03-10 14:53:13 +0100

    Revert "Adopt tfenv update"
    This reverts commit 41db8650adc392e2dd1ebd64b87e4363d1a58c07.

--- test/test_list.sh
+++ test/test_list.sh
@@ -1,51 +1,26 @@
 #!/usr/bin/env bash
 
-declare -a errors
+[ -n "$PKENV_DEBUG" ] && set -x
+source $(dirname $0)/helpers.sh
 
-function error_and_proceed() {
-  errors+=("${1}")
-  echo -e "pkenv: ${0}: Test Failed: ${1}" >&2
-}
-
-function error_and_die() {
-  echo -e "pkenv: ${0}: ${1}" >&2
-  exit 1
-}
-
-[ -n "${PKENV_DEBUG}" ] && set -x
-source $(dirname $0)/helpers.sh \
-  || error_and_die "Failed to load test helpers: $(dirname $0)/helpers.sh"
-
+echo ""
 echo "### List local versions"
-cleanup || error_and_die "Cleanup failed?!"
+cleanup
 
-for v in  0.8.0 0.8.6 0.9.0-rc2 0.9.0 0.11.0 0.12.2 0.12.3; do
-  pkenv install ${v} || error_and_proceed "Install of version ${v} failed"
+for v in 0.8.6 0.9.0-rc2 0.9.0 0.12.2 0.12.3; do
+  pkenv install ${v}
 done
 
 result=$(pkenv list)
 expected="$(cat << EOS
 0.12.3
 0.12.2
-0.11.0
 0.9.0
 0.9.0-rc2
 0.8.6
-0.8.0
 EOS
 )"
-
 if [ "${expected}" != "${result}" ]; then
-  error_and_proceed "List mismatch.\nExpected:\n${expected}\nGot:\n${result}"
-fi
-
-if [ ${#errors[@]} -gt 0 ]; then
-  echo -e "\033[0;31m===== The following list tests failed =====\033[0;39m" >&2
-  for error in "${errors[@]}"; do
-    echo -e "\t${error}"
-  done
+  echo "Expected: ${expected}, Got: ${result}" 1>&2
   exit 1
-else
-  echo -e "\033[0;32mAll list tests passed.\033[0;39m"
-fi;
-exit 0
+fi
